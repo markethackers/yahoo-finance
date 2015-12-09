0e88a134e9d70130f3549c201401e4ec931c8e62
Leo Brigadin <l.brigadin@paralect.com>
2015-12-10 00:13:23 +0300

    fix parse json response

--- lib/yahoo_finance.rb
+++ lib/yahoo_finance.rb
@@ -188,7 +188,7 @@ class YahooFinance
   def self.read_symbols(query)
     conn = open("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=#{query}&region=US&lang=en-US&callback=YAHOO.Finance.SymbolSuggest.ssCallback")
     result = conn.read
-    result.sub!('YAHOO.Finance.SymbolSuggest.ssCallback(', '').chomp!(')\;')
+    result.sub!('YAHOO.Finance.SymbolSuggest.ssCallback(', '').chomp!.chomp!(')\;')
     json_result = JSON.parse(result)
     json_result["ResultSet"]["Result"]
   end
